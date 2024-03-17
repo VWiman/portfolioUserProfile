25b18ce47481bfeb60e4b0739abb506aa3e34bdc
why_code <119089018+VWiman@users.noreply.github.com>
2024-03-17 14:20:51 +0100

    Rename 3

--- pages/layouts/Header.js
+++ pages/layouts/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
 			<header className="header">
-				<h1 className=" text-xl md:text-4xl md:pl-10">User Profile Card<span className="text-sm md:text-lg"> - 2024 Portfolio Project</span></h1>
+				<h1 className=" text-xl md:text-4xl md:pl-10">User Profile<span className="text-sm md:text-lg"> - 2024 Portfolio Project</span></h1>
 				<h2 className=" text-sm md:text-md md:pl-10">
 					Responsive website with create, read, update and delete created using NextJS and TailwindCSS
 				</h2>
